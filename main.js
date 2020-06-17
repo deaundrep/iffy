8f291666ea5e418c5aeeea195f73c1c8d645f71f
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-06-17 15:00:54 -0400

    done

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 // Your code here!
+const colorCombinator = require('./color-combinator.js')
+const colorDeconstructor = require('./color-deconstructor.js')
+const userInput1 = getInput(0)
+const userInput2 = getInput(1)
+const message = colorCombinator(userInput1 , userInput2);
+
+if (userInput1 !== 'undefined' && userInput2 === 'undefined') {
+  return colorCombinator;
+}
+
+colorDeconstructor()
+
+console.log(message);
 
 
 
